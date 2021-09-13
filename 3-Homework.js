afe93462f13593bcf6b99c8c4c0438544dce52d7
Cherisse Osorio <cherisseosorio@gmail.com>
2021-09-13 18:40:51 -0500

    adding initial HW assignment

---
+++ 3-Homework.js
@@ -0,0 +1,21 @@
+let pizzaToppings = ["Sausage", "Pepperoni", "Green Peppers", "Red Onions"];
+
+function welcome() {
+  return `Welcome ${name}!`;
+}
+function greetCustomer(callback) {
+  const name = promt ("Please Enter Your Name: ");
+  alert(callback(name));
+
+}
+greetCustomer(welcome)
+
+console.log(`We have an array of fresh toppings that include,
+${pizzaToppings}! Our toppings list is small but AMAZING!`);
+
+function getPizzaOrder()
+
+
+
+
+
